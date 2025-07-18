392936bd5442a8f407625d7dfcea53f1fd0144ee
Mridula S Rao <msrao@falabella.cl>
2025-07-19 01:43:01 +0530

    Foundation for all user routes

--- models/User.js
+++ models/User.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 //A Mongoose model is a wrapper on the Mongoose schema. A Mongoose schema defines the structure of the document, default values, validators, etc., whereas a Mongoose model provides an interface to the database for creating, querying, updating, deleting records, etc.
+//topic: what is mongoose and why do we need it
+//resource_attached: https://article.arunangshudas.com/why-use-mongoose-with-mongodb-67a026f66386
 
 const userSchema = new mongoose.Schema({
     name: String,
+    email: String,
+    password: String,
     uuid: crypto.randomUUID()
 })
 
-module.exports = mongoose.Model('Users', userSchema);
+module.exports = mongoose.model('Users', userSchema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,66 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router(); //topic: express Router
 //.Router() creates an mini-application with mdw and routes, but for a particular part of your project. So chat-app is a project, and within it with it's own routes is Users
 
 //registering user routes here:
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     console.log("User get route triggered");
+    //get user from req query -> search for user
+    //note: can userData be anything, not necessarily userName?
+    const user = await User.exists(`${userData}`).exec();
+    res.send(200).send(user);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     //need to attach createUser code with mongoDB here
+    try {
+        const { userName, userEmail, password } = req.body;
+        const newUser = await User.create({
+            name: userName,
+            email: userEmail,
+            password: password
+        });
+        res.status(200).send(newUser);
+    } catch {
+        //todo: make sure existing user is not created!!
+        console.log("Error occurred in creating the new user, try again");
+        res.status(500).json({ error: 'Failed to create user' });
+    }
+})
+
+router.put('/', async (req, res) => {
+    try {
+        //find the user with current userName and email then change it:
+        const { currentName, currentEmail } = req.body;
+        const user = await User.find({ name: `${currentName}`, email: `${currentEmail}` });
+        //todo: define updated name and email
+        user.name = updatedName;
+        user.email = updatedEmail;
+        user.save();
+        res.status(200).json({ response: 
+            { message: 'Updated successfully', 
+            body: user } 
+        });
+    } catch {
+        console.log('Failed to update userData');
+        res.status(500).json({ error: 'Failed to update the userData' })
+    }
+})
+
+router.delete('/', async (req, res) => {
+    try {
+        const { userName, userEmail } = req.body;
+        const newUser = await User.deleteOne({
+            name: userName,
+            email: userEmail
+        });
+        res.status(204).send('Resouce deleted successfully'); //note: gotta send this as json??
+    } catch {
+        console.log("Error occurred in deleting the user, try again");
+        res.status(500).json({ error: 'Failed to delete user' });
+    }
 })
 
 module.exports = router;
